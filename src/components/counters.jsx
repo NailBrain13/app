bef8f68469e40de58a6b32590c42fed8eab5a373
Алексей Казаков <75203434+NailBrain13@users.noreply.github.com>
2022-02-01 14:43:23 +0300

    In work

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -13,6 +13,12 @@ class Counters extends Component {
   render() {
     return (
       <React.Fragment>
+        <button
+          onClick={this.handleReset}
+          className='btn btn-primary btn-sm m-2'
+        >
+          Reset all counters
+        </button>
         {this.state.counters.map((counter) => (
           <Counter
             key={counter.id}
@@ -31,6 +37,14 @@ class Counters extends Component {
       (item) => item.id !== counterId,
     );
     this.setState({ counters: counters });
+  };
+
+  handleReset = () => {
+    const counters = this.state.counters.map((curentValue) => {
+      curentValue.value = 0;
+      return curentValue;
+    });
+    this.setState({ counters });
   };
 }
 
