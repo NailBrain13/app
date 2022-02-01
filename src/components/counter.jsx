2b1c35175903d9b43d90e7a4728ff94a4848a776
Алексей Казаков <75203434+NailBrain13@users.noreply.github.com>
2022-02-01 12:48:48 +0300

    Add btn

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,8 +1,22 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
+  state = {
+    count: 0,
+  };
+
   render() {
-    return <h1>Counter</h1>;
+    return (
+      <React.Fragment>
+        <span className='badge badge-primary m-2'>{this.formatCount()}</span>
+        <button className='btn btn-secondary btn-sm'>Add</button>
+      </React.Fragment>
+    );
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? 'Zero' : count;
   }
 }
 
