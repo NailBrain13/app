9490b2ddbb866a466cd54cfeb5a7ab021633bc33
Алексей Казаков <75203434+NailBrain13@users.noreply.github.com>
2022-02-02 05:09:49 +0300

    Destructing Args

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -3,21 +3,21 @@ import Counter from './counter';
 
 class Counters extends Component {
   render() {
+    const { onReset, counters, onDelete, onIncriment, onDicrement } =
+      this.props;
+
     return (
       <React.Fragment>
-        <button
-          onClick={this.props.onReset}
-          className='btn btn-primary btn-sm m-2'
-        >
+        <button onClick={onReset} className='btn btn-primary btn-sm m-2'>
           Reset all counters
         </button>
-        {this.props.counters.map((counter) => (
+        {counters.map((counter) => (
           <Counter
             key={counter.id}
             counter={counter}
-            onDelete={this.props.onDelete}
-            onIncriment={this.props.onIncriment}
-            onDicrement={this.props.onDicrement}
+            onDelete={onDelete}
+            onIncriment={onIncriment}
+            onDicrement={onDicrement}
           >
             <h4>Counter #{counter.id}</h4>
           </Counter>
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const NavBar = (props) => {
+const NavBar = ({ totalCounters }) => {
   return (
     <nav className='navbar navbar-light bg-light'>
       <a className='navbar-brand' href='!#'>
         Not zero counter{' '}
         <span className='badge badge-pill badge-secondary'>
-          {props.totalCounters}
+          {totalCounters}
         </span>
       </a>
     </nav>
